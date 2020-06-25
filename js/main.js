b5e87c96d267cf316f4a7435039d3e58e524486d
jscodebit <soujanya.janapatla@gmail.com>
2020-06-25 18:01:51 -0500

    Worked on Dialog box

--- js/main.js
+++ js/main.js
@@ -116,5 +116,24 @@
 			new WOW().init();	
 		/*END WOW ANIMATION JS*/
 
+		
+        var modal = document.querySelector(".modal");
+        var trigger = document.querySelector(".trigger");
+        var closeButton = document.querySelector(".close-button");
+    
+        function toggleModal() {
+           modal.classList.toggle("show-modal");
+        }
+    
+        function windowOnClick(event) {
+           if (event.target === modal) {
+               toggleModal();
+           }
+        }
+    
+        trigger.addEventListener("click", toggleModal);
+        closeButton.addEventListener("click", toggleModal);
+        window.addEventListener("click", windowOnClick);
+
 		     		
 })(jQuery);		
