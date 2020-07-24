3adae9900e17a00ba77c456018063d6c148acf90
jscodebit <soujanya.janapatla@gmail.com>
2020-07-24 01:57:04 -0500

    Added google form to the apply now button

--- js/main.js
+++ js/main.js
@@ -123,23 +123,23 @@
 		/*END WOW ANIMATION JS*/
 
 		
-        var modal = document.querySelector(".modal");
-        var trigger = document.querySelector(".trigger");
-        var closeButton = document.querySelector(".close-button");
+        // var modal = document.querySelector(".modal");
+        // var trigger = document.querySelector(".trigger");
+        // var closeButton = document.querySelector(".close-button");
     
-        function toggleModal() {
-           modal.classList.toggle("show-modal");
-        }
+        // function toggleModal() {
+        //    modal.classList.toggle("show-modal");
+        // }
     
-        function windowOnClick(event) {
-           if (event.target === modal) {
-               toggleModal();
-           }
-        }
+        // function windowOnClick(event) {
+        //    if (event.target === modal) {
+        //        toggleModal();
+        //    }
+        // }
     
-        trigger.addEventListener("click", toggleModal);
-        closeButton.addEventListener("click", toggleModal);
-		window.addEventListener("click", windowOnClick);
+        // trigger.addEventListener("click", toggleModal);
+        // closeButton.addEventListener("click", toggleModal);
+		// window.addEventListener("click", windowOnClick);
 		
 		$("#testimonial-slider").owlCarousel({
 			items:1,
